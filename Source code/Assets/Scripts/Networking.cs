5409e63a21fb1f157e93846eb264947d10adfa25
ursuvladandrei <ursuvladandrei@gmail.com>
2016-11-07 21:47:41 +0200

    Added random scene generation and reset scene functionality

--- Source code/Assets/Scripts/Networking.cs
+++ Source code/Assets/Scripts/Networking.cs
@@ -43,21 +43,30 @@ public class Networking : MonoBehaviour {
         SpawnPlayer();
     }
 
+    private void ResetGame() {
+        Application.LoadLevel(Application.loadedLevel);
+        Network.Disconnect();
+    }
+ 
     void OnGUI() {
         if (!Network.isClient && !Network.isServer) {
-            if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
+            if (GUI.Button(new Rect(10, 60, 100, 50), "Start Server"))
                 StartServer();
 
-            if (GUI.Button(new Rect(100, 250, 250, 100), "Refresh Hosts"))
+            if (GUI.Button(new Rect(10, 110, 100, 50), "Refresh Hosts"))
                 RefreshHostList();
 
             if (hostList != null) {
                 for (int i = 0; i < hostList.Length; i++) {
-                    if (GUI.Button(new Rect(400, 100 + (110 * i), 300, 100), hostList[i].gameName))
+                    if (GUI.Button(new Rect(10, 160 + (50 * i), 100, 50), hostList[i].gameName))
                         JoinServer(hostList[i]);
                 }
             }
         }
+        // Add reset scene button
+        if (GUI.Button(new Rect(10, 10, 100, 50), "Reset Game"))
+            ResetGame();
+
     }
 
 	// Use this for initialization
---
+++ Source code/Assets/Scripts/SceneGeneration.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class SceneGeneration : MonoBehaviour
+{
+
+    public Transform brick;
+    public static int width = 19;
+    public static int height = 27;
+    public static int limit_w = (width - 1) / 2;
+    public static int limit_h = (height - 1) / 2;
+    bool[,] grid = new bool[height, width];
+    public static System.Random rnd = new System.Random();
+
+    public void generateStructure(int x, int y)
+    {
+        int[] xs = new int[4];
+        int[] ys = new int[4];
+
+        xs[0] = x - 1; ys[0] = y;
+        xs[1] = x; ys[1] = y - 1;
+        xs[2] = x + 1; ys[2] = y;
+        xs[3] = x; ys[3] = y + 1;
+
+        int rnd_no = rnd.Next(0, 4);
+        int chosen_x = xs[rnd_no];
+        int chosen_y = ys[rnd_no];
+
+        for (int i = (x - 1); i <= (x + 1); i++)
+            for (int j = (y - 1); j <= (y + 1); j++)
+            {
+                if ((i == chosen_x) && (j == chosen_y))
+                    continue;
+
+                if (i == x && j == y)
+                    continue;
+                Instantiate(brick, new Vector3(i, j, 1), Quaternion.identity);
+                grid[i + limit_h, j + limit_w] = true;
+            }
+
+
+    }
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        // Generate outer grid
+        for (int i = -limit_h; i <= limit_h; i++)
+        {
+            grid[(i + limit_h), 0] = true;
+            Instantiate(brick, new Vector3(i, -limit_w, 1), Quaternion.identity);
+            grid[(i + limit_h), (width - 1)] = true;
+            Instantiate(brick, new Vector3(i, limit_w, 1), Quaternion.identity);
+        }
+
+        for (int j = -limit_w; j <= limit_w; j++)
+        {
+            grid[0, (j + limit_w)] = true;
+            Instantiate(brick, new Vector3(-limit_h, j, 1), Quaternion.identity);
+            grid[(height - 1), (j + limit_w)] = true;
+            Instantiate(brick, new Vector3(limit_h, j, 1), Quaternion.identity);
+        }
+
+        for (int i = (-limit_h + 3); i <= (limit_h - 3); i += 4)
+            for (int j = (-limit_w + 3); j <= (limit_w - 3); j += 4)
+                generateStructure(i, j);
+    }
+
+    /*
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    */
+}
